917ff31444633a8e23274e67d54f1ee8f695972b
Artem <raaauder@yandex.ru>
2020-03-23 17:13:23 +0300

    fix

--- data_remote/src/main/java/msk/pobazar/wcquiz/data_remote/api/ImageApi.kt
+++ data_remote/src/main/java/msk/pobazar/wcquiz/data_remote/api/ImageApi.kt
@@ -5,7 +5,7 @@ import io.reactivex.Observable
 
 interface ImageApi {
 
-    fun getUri(url: List<String>): Observable<List<String>>
+    fun getUri(urls: List<String>): Observable<List<String>>
 
     fun loadImage(uri: String): Observable<Completable>
 }
