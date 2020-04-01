d45658b811715594ce511b4cc289aad418521066
Artem <raaauder@yandex.ru>
2020-04-01 20:42:31 +0300

    fix

--- feature_mods/src/main/java/msk/pobazar/wcquiz/feature_mods/presenter/ModsPresenter.kt
+++ feature_mods/src/main/java/msk/pobazar/wcquiz/feature_mods/presenter/ModsPresenter.kt
@@ -5,22 +5,13 @@ import msk.pobazar.wcquiz.core.base.BasePresenter
 import msk.pobazar.wcquiz.core.navigation.Router
 import msk.pobazar.wcquiz.core.navigation.screens.NavigationScreen
 import msk.pobazar.wcquiz.core.navigation.transitionsParams.GameParams
-import msk.pobazar.wcquiz.domain.interactor.ThemeInteractor
 import msk.pobazar.wcquiz.domain.model.Theme
 import javax.inject.Inject
 
 @InjectViewState
 class ModsPresenter @Inject constructor(
-    private val router: Router,
-    private val themeInteractor: ThemeInteractor
+    private val router: Router
 ) : BasePresenter<ModsView>() {
-
-    override fun onFirstViewAttach() {
-        super.onFirstViewAttach()
-        for (theme in themeInteractor.getAll()) {
-            viewState.addMod(theme)
-        }
-    }
 
     fun onGameClick(theme: Theme) {
         router.replace(NavigationScreen.Game(GameParams(theme)))
